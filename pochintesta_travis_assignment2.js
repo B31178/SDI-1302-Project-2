c523f92997a3ffe2f307e753e359e6d636a65fb4
B31178 <travisp@fullsail.edu>
2013-02-12 23:13:47 -0500

    1st var & functions added.

--- pochintesta_travis_assignment2.js
+++ pochintesta_travis_assignment2.js
@@ -3,6 +3,55 @@
 //	Project 2
 //	Functions & Loops
 
+//  Variables
+
+var computerOn = true;
+var monitorOn = false;
+var allOn = true;
+
+var tech = ["Moss", "Roy", "Jan"];
+
+var tier1 = [1, 2, 3, 4];
+var tier2 = [5, 6, 7, 8];
+var tier3 = [9, 10, 11, 12];
+
+var floor = 7;
+
+var name = "Roy";
+
 //  Functions
 
+/*Boolean Function*/
+var isOn = function(computerOn, monitorOn, allOn) {
+	
+	if((computerOn && monitorOn) || allOn) {
+			return "Excellent!  Now we just need a little more information to complete your ticket.";
+	} else {
+			return "Please do so now.";
+	}
+	
+/*Number Function*/
+var whichFloor = function(tier1, tier2, tier3) {
+
+	if floor = ( tier1(0) || tier1(1) || tier1(2) || tier1(3) );
+		while (floor <= 12) {
+			console.log "Determining support tier.";
+	} else {
+			console.log "Come down from the roof.";	
+	
+/*Array Function*/
+var whichTech = function(tech(0), tech(1), tech(2)) {
+	if 
+	
+/*String Function*/
+var techName = function(name, reply) {
+	
+	var arrive = " will arrive shortly.";
+	
+	if name = tech(1);
+			return name + arrive;
+	} else {
+			return "A tech is on the way."
+	}
+	
 //  Main Code
